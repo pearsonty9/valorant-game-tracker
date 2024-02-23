d5547f2ec8f503eb908e06d46e9803d53f53b132
Tyler Pearson <pearsonty9@gmail.com>
2024-02-23 18:00:48 -0500

    fix: uploading larger video files

--- api_client.py
+++ api_client.py
@@ -1,12 +1,33 @@
 import os
 import re
-from pathlib import Path
+import http.client as httplib
+import httplib2
+import random
+import time
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
 
-# FIle should be refactored into using a class and have authentication be done on initialization
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+  httplib.IncompleteRead, httplib.ImproperConnectionState,
+  httplib.CannotSendRequest, httplib.CannotSendHeader,
+  httplib.ResponseNotReady, httplib.BadStatusLine)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+# File should be refactored into using a class and have authentication be done on initialization
 
 def get_sheet_data(credentials, spreadsheetId, range):
     with build('sheets', 'v4', credentials=credentials) as service:
@@ -51,36 +72,73 @@ def get_last_row_from_spreadsheet(credentials, spreadsheetId):
     response = get_sheet_data(credentials, spreadsheetId, f"A${lastrow}:${lastcolumn}${lastrow}")
     return response[0]
 
-def upload_to_youtube(credentials, video_path, title, description, privacyStatus):
-    api_service_name = "youtube"
-    api_version = "v3"
-    youtube = build(api_service_name, api_version, credentials=credentials)
-
-    request = youtube.videos().insert(
-        part="snippet, status",
-        body={
-            "snippet": {
-                "title": title,
-                "description": description
-            },
-            "status": {"privacyStatus": privacyStatus}
-        },
-        media_body=MediaFileUpload(video_path)
+def initialize_upload(credentials, options):
+  body=dict(
+    snippet=dict(
+      title=options["title"],
+      description=options["description"],
+    ),
+    status=dict(
+      privacyStatus=options["privacyStatus"]
     )
-    response = request.execute()
-    return response
+  )
+
+  # Call the API's videos.insert method to create and upload the video.
+  with build('youtube', 'v3', credentials=credentials) as youtube:
+    insert_request = youtube.videos().insert(
+        part=','.join(body.keys()),
+        body=body,
+        media_body=MediaFileUpload(options["file"], chunksize=-1, resumable=True)
+    )
+
+    return resumable_upload(insert_request)
+
+# This method implements an exponential backoff strategy to resume a
+# failed upload.
+def resumable_upload(request):
+  response = None
+  error = None
+  retry = 0
+  while response is None:
+    try:
+      print('Uploading file...')
+      status, response = request.next_chunk()
+      if response is not None:
+        if 'id' in response:
+          print('Video id "%s" was successfully uploaded.' % response['id'])
+          return response['id']
+        else:
+          exit('The upload failed with an unexpected response: %s' % response)
+    except HttpError as e:
+      if e.resp.status in RETRIABLE_STATUS_CODES:
+        error = 'A retriable HTTP error %d occurred:\n%s' % (e.resp.status,
+                                                             e.content)
+      else:
+        raise
+    except RETRIABLE_EXCEPTIONS as e:
+      error = 'A retriable error occurred: %s' % e
+
+    if error is not None:
+      print(error)
+      retry += 1
+      if retry > MAX_RETRIES:
+        exit('No longer attempting to retry.')
+
+      max_sleep = 2 ** retry
+      sleep_seconds = random.random() * max_sleep
+      print('Sleeping %f seconds and then retrying...' % sleep_seconds)
+      time.sleep(sleep_seconds)
 
 def authenticate_client_user():
-    scopes = ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/youtube.upload", "https://www.googleapis.com/auth/spreadsheets"]
+    SCOPES = ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/youtube.upload", "https://www.googleapis.com/auth/spreadsheets"]
+    CLIENT_SECRETS_FILE = "client_secret_494135269922-03i1gfsobfjj1ei86pu75sdmmcfa3ojg.apps.googleusercontent.com.json"
 
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    client_secrets_file = "client_secret_494135269922-03i1gfsobfjj1ei86pu75sdmmcfa3ojg.apps.googleusercontent.com.json"
-
     # Get credentials and create an API client
-    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_local_server()
 
     return credentials
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import re
 
-from videos import group_files_by_created_date, sort_files_into_folders, get_file_paths
-from games import associate_titles_with_videos, get_video_titles_from_spreadsheet, get_game_data, get_video_title
-from api_client import upload_to_youtube, append_sheet_data, authenticate_client_user, get_last_row_from_spreadsheet, update_sheet_data
+from videos import get_file_paths
+from games import get_game_data, get_video_title
+from api_client import append_sheet_data, authenticate_client_user, get_last_row_from_spreadsheet, update_sheet_data, initialize_upload
 
 SPREADSHEET_ID = "1oV42lMxnIw2hfBfezyqv60j4Ps6Dfze_lvJWd9rz0Kw"
 
@@ -10,32 +10,26 @@ if __name__ == '__main__':
     credentials = authenticate_client_user()
     
     while (True):
-        print("")
-        print("add: create a new game entry")
-        user_input = input("> ")
-
-        if user_input == "add":
-            previous_game_metadata = get_last_row_from_spreadsheet(credentials, "1oV42lMxnIw2hfBfezyqv60j4Ps6Dfze_lvJWd9rz0Kw")
-            game_metadata = get_game_data(previous_game_metadata)
-            inserted_data_response = append_sheet_data(credentials, "1oV42lMxnIw2hfBfezyqv60j4Ps6Dfze_lvJWd9rz0Kw", "Episode 8 Act 1", [game_metadata])
-            # Find video file to upload
-            files = get_file_paths("d:\\GameReplays")
-            most_recent_video = files[-1]
-            # Create title for video using game data
-            title = get_video_title(game_metadata)
-            # Upload video to youtube
-            if game_metadata[-1] == "Yes":
-                response = input(f'Upload \033[1m{most_recent_video}\033[0m with title "\033[1m{title}\033[0m" (y/n): ')
-                if response == "y":
-                    print("Uploading video to YouTube...")
-                    upload_response = upload_to_youtube(credentials, most_recent_video, title, "", "private")
-                    # Update game data spreadsheet with video link 
-                    p = re.compile('^.*![A-Z]+\\d+:([A-Z]+)(\\d+)$')
-                    match = p.match(inserted_data_response["tableRange"])
-                    lastcolumn = match.group(1)
-                    lastrow = match.group(2)
-                    update_sheet_data(credentials, SPREADSHEET_ID, f"{chr(ord(lastcolumn) - 3)}{int(lastrow)+1}", [[f"https://youtu.be/{upload_response["id"]}"]])
-        elif user_input == "test":
-            pass
-        else:
-            exit()
+        previous_game_metadata = get_last_row_from_spreadsheet(credentials, "1oV42lMxnIw2hfBfezyqv60j4Ps6Dfze_lvJWd9rz0Kw")
+        game_metadata = get_game_data(previous_game_metadata)
+        inserted_data_response = append_sheet_data(credentials, "1oV42lMxnIw2hfBfezyqv60j4Ps6Dfze_lvJWd9rz0Kw", "Episode 8 Act 1", [game_metadata])
+        # Find video file to upload
+        files = get_file_paths("d:\\GameReplays")
+        most_recent_video = files[-1]
+        # Create title for video using game data
+        title = get_video_title(game_metadata)
+        # Upload video to youtube
+        if game_metadata[-1] == "Yes":
+            response = input(f'Upload \033[1m{most_recent_video}\033[0m with title "\033[1m{title}\033[0m" (y/n): ')
+            if response == "y":
+                try:
+                    print("Uploading video to YouTube...")                
+                    upload_response = initialize_upload(credentials, {"title": title, "description": "", "file": most_recent_video, "privacyStatus": "private"})
+                except Exception as e:
+                    print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+                # Update game data spreadsheet with video link 
+                p = re.compile('^.*![A-Z]+\\d+:([A-Z]+)(\\d+)$')
+                match = p.match(inserted_data_response["tableRange"])
+                lastcolumn = match.group(1)
+                lastrow = match.group(2)
+                update_sheet_data(credentials, SPREADSHEET_ID, f"{chr(ord(lastcolumn) - 3)}{int(lastrow)+1}", [[f"https://youtu.be/{upload_response["id"]}"]])  
